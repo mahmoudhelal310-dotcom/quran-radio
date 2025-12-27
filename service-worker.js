22ad77c3a5896bd46a6ac255fad2903995541bdf
mahmoudhelal310-dotcom <mahmoudhelal310@gmail.com>
2025-12-27 12:09:29 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker Installed');
-});
-
-self.addEventListener('fetch', function(event) {
-  // يمكنك إضافة caching لاحقًا
-});
